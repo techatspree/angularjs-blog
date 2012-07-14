a6aa1e2d03137fbb817c5963b5b3dc0463c19e96
Heinz Wilming <heinz.wilming@akquinet.de>
2012-07-14 21:18:33 +0200

    comment crud rest endpoints

--- mobile-blog-rest/src/it-test/java/de/akquinet/aerogear/rest/CommentServiceTest.java
+++ mobile-blog-rest/src/it-test/java/de/akquinet/aerogear/rest/CommentServiceTest.java
@@ -3,6 +3,7 @@ package de.akquinet.aerogear.rest;
 import static de.akquinet.aerogear.rest.deployment.TestWebArchiveDeployment.URL;
 
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response.Status;
 
 import junit.framework.Assert;
@@ -50,5 +51,59 @@ public class CommentServiceTest {
 		Assert.assertEquals(Long.valueOf(24), comment.getId());
 		Assert.assertNotNull(comment.getAuthor());
 	}
+
+	@Test
+	public void testUpdateComment() throws Exception {
+
+		ClientRequest request = new ClientRequest(URL + "/blog/6/comment/24");
+		request.accept(MediaType.APPLICATION_JSON);
+		MultivaluedMap<String, String> formParameters = request
+				.getFormParameters();
+
+		final String content = "new content";
+		formParameters.add("content", content);
+
+		ClientResponse<Comment> response = request.put(Comment.class);
+
+		Assert.assertEquals(Status.OK.getStatusCode(), response.getStatus());
+
+		Comment entity = response.getEntity();
+		Assert.assertEquals(content, entity.getContent());
+	}
+
+	@Test
+	public void testUpdateComment_ConstraintViolations() throws Exception {
+
+		ClientRequest request = new ClientRequest(URL + "/blog/6/comment/24");
+		request.accept(MediaType.APPLICATION_JSON);
+
+		final MultivaluedMap<String, String> formParameters = request
+				.getFormParameters();
+
+		final String content = "";
+		formParameters.add("content", content);
+
+		ClientResponse<Comment> response = request.put(Comment.class);
+
+		Assert.assertEquals(Status.CONFLICT.getStatusCode(), response.getStatus());
+	}
+
+
+	@Test
+	public void testDeleteBlogEntry() throws Exception {
+		ClientRequest request = new ClientRequest(URL + "/blog/7/comment/26");
+		request.accept(MediaType.APPLICATION_JSON);
+
+		ClientResponse<Comment> response = request.get(Comment.class);
+
+		Assert.assertEquals(Status.OK.getStatusCode(), response.getStatus());
+		Comment comment = response.getEntity();
+
+		ClientRequest deleteRequest = new ClientRequest(URL + "/blog/7/comment/" + comment.getId());
+		ClientResponse<?> deleteResponse = deleteRequest.delete();
+
+		Assert.assertEquals(Status.NO_CONTENT.getStatusCode(), deleteResponse.getStatus());
+	}
+
 
 }
--- mobile-blog-rest/src/main/java/de/akquinet/aerogear/rest/CommentService.java
+++ mobile-blog-rest/src/main/java/de/akquinet/aerogear/rest/CommentService.java
@@ -5,12 +5,19 @@ import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.validation.ValidationException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import de.akquinet.aerogear.BlogEntry;
 import de.akquinet.aerogear.Comment;
 import de.akquinet.aerogear.dao.BlogEntryDao;
 import de.akquinet.aerogear.dao.CommentDao;
@@ -28,14 +35,12 @@ public class CommentService {
 	@Inject
 	private CommentDao commentDao;
 
-
 	@Inject
 	private ContraintValidator validator;
 
 	@GET
 	@Produces({ MediaType.APPLICATION_JSON, MediaType.TEXT_XML })
-	public List<Comment> getResultList(
-			@PathParam("blogId") final long blogId) {
+	public List<Comment> getResultList(@PathParam("blogId") final long blogId) {
 		return commentDao.findComments(blogId);
 	}
 
@@ -46,6 +51,35 @@ public class CommentService {
 		final Comment comment = commentDao.find(id);
 		log.info("findComment(" + id + ") " + comment);
 		return comment;
+	}
+
+	@DELETE
+	@Path("/{commentId:[1-9][0-9]*}")
+	public void deleteComment(@PathParam("commentId") final long id) {
+		log.info("delete comment " + id);
+		commentDao.remove(id);
+	}
+
+	@PUT
+	@Path("/{commentId:[1-9][0-9]*}")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.TEXT_XML })
+	public Comment updateBlogEntry(@PathParam("commentId") final long id,
+			@FormParam("content") final String content)
+			throws ValidationException {
+		final Comment comment = commentDao.find(id);
+		comment.setContent(content);
+		validator.validate(comment);
+
+		return comment;
+
+	}
+
+	@POST
+	@Consumes({ MediaType.APPLICATION_JSON, MediaType.TEXT_XML })
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.TEXT_XML })
+	public Comment saveBlogEntry(@PathParam("blogId") final long blogEntryId, final Comment comment) {
+		BlogEntry blogEntry = blogEntryDao.find(blogEntryId);
+		return null;
 	}
 
 }
