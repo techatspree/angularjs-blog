d96de9f35302f55449ec533215ab0f9afdc17b1b
Philipp Kumar <philipp.kumar@akquinet.de>
2012-07-17 08:13:30 +0200

    Cleanup

--- mobile-blog-web/src/main/webapp/js/DesktopApp.js
+++ mobile-blog-web/src/main/webapp/js/DesktopApp.js
@@ -1,29 +1,29 @@
 App = {
-		fetchTemplates: function() {
-            $.ajax({
-                url: "../js/template/BlogListEntry.tmpl",
-                dataType: "html",
-                success: function(data) {
-                    $("head").append(data);
-                }
-            });
+	fetchTemplates: function() {
+        $.ajax({
+            url: "../js/template/BlogListEntry.tmpl",
+            dataType: "html",
+            success: function(data) {
+                $("head").append(data);
+            }
+        });
 
-            $.ajax({
-                url: "../js/template/BlogEntry.tmpl",
-                dataType: "html",
-                success: function(data) {
-                    $("head").append(data);
-                }
-            });
+        $.ajax({
+            url: "../js/template/BlogEntry.tmpl",
+            dataType: "html",
+            success: function(data) {
+                $("head").append(data);
+            }
+        });
 
-            $.ajax({
-                url: "../js/template/Comment.tmpl",
-                dataType: "html",
-                success: function(data) {
-                    $("head").append(data);
-                }
-            });
-    	},
+        $.ajax({
+            url: "../js/template/Comment.tmpl",
+            dataType: "html",
+            success: function(data) {
+                $("head").append(data);
+            }
+        });
+	},
 
 	load: function() {
 	    App.fetchTemplates();
@@ -32,10 +32,10 @@ App = {
                     params = searchString.split("&"),
                     keyValueHash = {};
 
-                  for (var i = 0; i < params.length; i++) {
-                    var val = params[i].split("=");
-                    keyValueHash[unescape(val[0])] = unescape(val[1]);
-                  }
+	    for (var i = 0; i < params.length; i++) {
+	        var val = params[i].split("=");
+	        keyValueHash[unescape(val[0])] = unescape(val[1]);
+	    }
 
         var postID = keyValueHash['showPost'];
 
