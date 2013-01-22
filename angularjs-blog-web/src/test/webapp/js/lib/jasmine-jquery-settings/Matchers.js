4619d74454b7d7b9a908b387f5c04bf719a8584b
Till Hermsen <till.hermsen@akquinet.de>
2013-01-22 14:11:53 +0100

    deleted matchers.js

--- angularjs-blog-web/src/test/webapp/js/lib/jasmine-jquery-settings/Matchers.js
+++
@@ -1,8 +0,0 @@
-//
-//jasmine.Matchers.prototype.toEqualData = function(expected) {
-//    return angular.equals(this.actual, expected);
-//};
-//jasmine.Matchers.prototype.toEqualData = jasmine.Matchers.matcherFn_(
-//    'toEqualData',
-//    jasmine.Matchers.prototype.toEqualData
-//);
